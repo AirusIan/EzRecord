65591d5c8319266f24405848a5df426a5b8eda4e
Airus Ian <seang38077@gmail.com>
2021-07-14 21:35:17 +0800

    完成編輯畫面的樂譜顯示(功能未實作)

--- app/src/main/java/com/midisheetmusic/EditorActivity.java
+++ app/src/main/java/com/midisheetmusic/EditorActivity.java
@@ -1,20 +1,120 @@
 package com.midisheetmusic;
 
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.midisheetmusic.R;
+import com.midisheetmusic.drawerItems.ExpandableSwitchDrawerItem;
+import com.midisheetmusic.sheets.ClefSymbol;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondarySwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
 
-public class EditorActivity extends AppCompatActivity {
+import java.util.zip.CRC32;
+
+public class EditorActivity extends  MidiHandlingActivity {
+    public static final String MidiTitleID = "MidiTitleID";
+    private MidiPlayer player;   /* The play/stop/rewind toolbar */
+    private Piano piano;         /* The piano at the top */
+    private SheetMusic sheet;    /* The sheet music */
+    private LinearLayout layout; /* The layout */
+    private MidiFile midifile;   /* The midi file to play */
+    private MidiOptions options; /* The options for sheet music and sound */
+    private Uri uri;
+    private String title;
+    private long midiCRC;
     public void onCreate(Bundle state) {
         try {
             super.onCreate(state);
             setTitle("MidiSheetMusic: Welcome");
             // Load the list of songs
             setContentView(R.layout.editor_bar);
-        } catch (Exception e) {
+            hideSystemUI();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            uri = this.getIntent().getData();
+            if (uri == null) {
+                this.finish();
+                return;
+            }
+            title = this.getIntent().getStringExtra(MidiTitleID);
+            if (title == null) {
+                title = uri.getLastPathSegment();
+            }
+            FileUri file = new FileUri(uri, title);
+            this.setTitle("MidiSheetMusic: " + title);
+            byte[] data;
+
+            data = file.getData(this);
+            midifile = new MidiFile(data, title);
+
+            options = new MidiOptions(midifile);
+            CRC32 crc = new CRC32();
+            crc.update(data);
+            midiCRC = crc.getValue();
+            createViews();
+
+
+            //***********
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public void createViews() {
+        layout = findViewById(R.id.editor_content);
+
+        player = new MidiPlayer(this);
+        player.setSheetUpdateRequestListener(() -> createSheetMusic(options));
+        createSheetMusic(options);
+    }
+    private void
+    createSheetMusic(MidiOptions options) {
+        if (sheet != null) {
+            layout.removeView(sheet);
+        }
+        sheet = new SheetMusic(this);
+        sheet.init(midifile, options);
+        sheet.setPlayer(player);
+        layout.addView(sheet);//將樂譜加入畫面
+        player.SetMidiFile(midifile, options, sheet);
+        player.updateToolbarButtons();
+        layout.requestLayout();
+        sheet.draw();
+    }
+    private void hideSystemUI() {
+        // Enables sticky immersive mode.
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
+    void OnMidiDeviceStatus(boolean connected) {
+
+
+    }
+
+    @Override
+    void OnMidiNote(int note, boolean pressed) {
+
     }
 }
